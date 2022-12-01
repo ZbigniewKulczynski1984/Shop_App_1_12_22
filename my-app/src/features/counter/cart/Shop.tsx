0ef84663a42009d10d560246ce42642410dfbb48
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-01 20:12:54 +0100

    code added

---
+++ my-app/src/features/counter/cart/Shop.tsx
@@ -0,0 +1,25 @@
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {  addItem, Item, selectItemsQuantity } from './cartSlice';
+
+export function Shop() {
+    const dispatch = useAppDispatch();
+    const quantity = useAppSelector(selectItemsQuantity);
+
+    const product: Item = {
+        id: '1',
+        name: 'Call of Duty',
+        price: 150
+    }
+
+    return (
+        <div>
+            <div id="cart" className="cart">
+                {quantity}
+            </div>
+
+            <div className="product">
+                <button onClick={() => {dispatch(addItem(product))}}>Add product</button>
+            </div>
+        </div>
+    )
+}
