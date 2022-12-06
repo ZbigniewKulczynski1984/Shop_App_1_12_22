670beef26ba1d6fa02c2a94704083b7ffc3ab2cb
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-06 18:46:08 +0100

    code added

--- my-app/src/features/cart/Shop.tsx
+++ my-app/src/features/cart/Shop.tsx
@@ -1,14 +1,16 @@
 import { Cart } from "./Cart";
 import './Shop.css';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { loadProducts, ProductModel, selectSearchResults } from "../product/productsSlice";
+import { loadProducts, ProductModel, selectIsLoading, selectSearchResults } from "../product/productsSlice";
 import { Product } from "../product/Product";
 import { AlertList } from "../notifications/AlertList";
 import { useEffect } from "react";
 
 export function Shop() {
   const products: ProductModel[] = useAppSelector(selectSearchResults);
+  const isLoading: boolean = useAppSelector(selectIsLoading);
   const dispatch = useAppDispatch();
+  
 
   useEffect(() => {
     dispatch(loadProducts());
@@ -19,6 +21,12 @@ export function Shop() {
 			<Cart />
 
 			<div className="container pt-5">
+				{ isLoading ? <div className="text-center">
+  					<div className="spinner-border" role="status">
+    				<span className="visually-hidden">Loading...</span>
+  					</div>
+				</div>: 
+
 				<div className="products-list mb-5">
 					{products.map((product, key) => (
 						<Product
@@ -29,7 +37,7 @@ export function Shop() {
 						/>
 					))}
 				</div>
-
+}
 				<AlertList />
 			</div>
 		</div>
