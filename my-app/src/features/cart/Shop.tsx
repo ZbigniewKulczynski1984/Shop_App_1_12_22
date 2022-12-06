241fe95703edfdb10a427a0945ff3a643b294e3a
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-06 21:25:38 +0100

    code added, icons installed

--- my-app/src/features/cart/Shop.tsx
+++ my-app/src/features/cart/Shop.tsx
@@ -1,41 +1,43 @@
-import { Cart } from "./Cart";
+import { Cart } from './Cart';
 import './Shop.css';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { loadProducts, ProductModel, selectIsLoading, selectSearchResults } from "../product/productsSlice";
-import { Product } from "../product/Product";
-import { AlertList } from "../notifications/AlertList";
-import { useEffect } from "react";
+import {
+	loadProducts,
+	ProductModel,
+	selectIsLoading,
+	selectSearchResults,
+} from '../product/productsSlice';
+import { Product } from '../product/Product';
+import { AlertList } from '../notifications/AlertList';
+import { useEffect } from 'react';
 
 export function Shop() {
-  const products: ProductModel[] = useAppSelector(selectSearchResults);
-  const isLoading: boolean = useAppSelector(selectIsLoading);
-  const dispatch = useAppDispatch();
-  
+	const products: ProductModel[] = useAppSelector(selectSearchResults);
+	const isLoading: boolean = useAppSelector(selectIsLoading);
+	const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(loadProducts());
-  }, []);
+	useEffect(() => {
+		dispatch(loadProducts());
+	}, []);
 
-    return (
-        <div className="position-relative">
+	return (
+		<div className="position-relative">
 			<Cart />
 
 			<div className="container pt-5">
-				{ isLoading ? <div className="text-center">
-  					<div className="spinner-border" role="status">
-    				<span className="visually-hidden">Loading...</span>
-  					</div>
-				</div>: 
-
-				<div className="products-list mb-5">
-					{products.map((product, key) => (
-						<Product 
-						key={key}
-						product={product}
-						/>
-					))}
-				</div>
-}
+				{isLoading ? (
+					<div className="text-center">
+						<div className="spinner-border" role="status">
+							<span className="visually-hidden">Loading...</span>
+						</div>
+					</div>
+				) : (
+					<div className="products-list mb-5">
+						{products.map((product, key) => (
+							<Product key={key} product={product} />
+						))}
+					</div>
+				)}
 				<AlertList />
 			</div>
 		</div>
--- my-app/src/features/product/Product.tsx
+++ my-app/src/features/product/Product.tsx
@@ -1,24 +1,23 @@
 import { useAppDispatch } from '../../app/hooks';
 import { addNotification } from '../notifications/notificationSlice';
 import { addItem, Item } from '../cart/cartSlice';
-import { stringify } from 'querystring';
+// import { stringify } from 'querystring';
 import { ProductModel } from './productsSlice';
-// import './Procuct.css';
+import '../product/Product.css';
 
 export interface ProductProps {
-	product: ProductModel
+	product: ProductModel;
 }
 
 export function Product(props: ProductProps) {
 	const dispatch = useAppDispatch();
-	const {product} = props;
+	const { product } = props;
 
 	const item: Item = {
 		id: product.id,
 		name: product.name,
 		price: product.price,
 		quantity: 1,
-
 	};
 
 	const handleClick = () => {
@@ -33,15 +32,14 @@ export function Product(props: ProductProps) {
 
 	return (
 		<div className="card">
-			<div className='d-flex justify-content-center align-item-center card-img-top'>
-			<img className='h-75' src={product.image} alt={product.name} />
+			<div className="d-flex justify-content-center align-items-center product-image-wrapper">
+				<img className="product-image" src={product.image} alt={product.name} />
 			</div>
 			<div className="card-body">
 				<h5 className="card-title">{product.name}</h5>
 			</div>
-			<div className='card-footer d-flex justyfy-content-between align-items-center'>
-
-				<strong className="product-price fs-4">{product.price}</strong>
+			<div className="card-footer d-flex justify-content-between align-items-center">
+				<strong className="fs-4">{product.price}</strong>
 
 				<button className="btn btn-primary" onClick={handleClick}>
 					Add product
